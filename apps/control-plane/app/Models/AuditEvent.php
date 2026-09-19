13d39de7b0540dc038e8d28bf50ead915cfef096
Muhammad Sohail <msohailbuzdar42@gmail.com>
2026-09-19 16:36:22 +0500

    Update AuditEvent.php

--- apps/control-plane/app/Models/AuditEvent.php
+++ apps/control-plane/app/Models/AuditEvent.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class AuditEvent extends Model
-{
-    //
+class AuditEvent extends Model {
+    use HasUuids;
+    protected $fillable = ['project_id', 'action', 'context'];
+    protected $casts = ['context' => 'array'];
 }
