29622dd1d8ba551d73dcb6639a08961f85dcee36
Muhammad Sohail <msohailbuzdar42@gmail.com>
2026-09-19 16:36:27 +0500

    Update ImportSchema.php

--- apps/control-plane/app/Models/ImportSchema.php
+++ apps/control-plane/app/Models/ImportSchema.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class ImportSchema extends Model
-{
-    //
+class ImportSchema extends Model {
+    use HasUuids;
+    protected $fillable = ['project_id', 'schema_name', 'definition', 'version'];
+    protected $casts = ['definition' => 'array'];
+    public function project() { return $this->belongsTo(Project::class); }
 }
